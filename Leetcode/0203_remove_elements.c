c1096a40c6f0f057e79f8b812cd37c6457c11110
gongpingzz <gongpingzz@foxmail.com>
2023-02-26 21:05:33 +0800

    添加203题解，添加哑节点解决头节点可能需要被删除问题

---
+++ Leetcode/0203_remove_elements.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    /* 为解决头节点可能需要删除的问题，创建一个哑节点 */
+    struct ListNode* dummyHead = malloc(sizeof(struct ListNode));
+    dummyHead->next = head;
+    struct ListNode* temp = dummyHead;
+    while (temp->next != NULL) {
+        if (temp->next->val == val) {
+            temp->next = temp->next->next;
+        } else {
+            temp = temp->next;
+        }
+    }
+    return dummyHead->next;
+
+}
