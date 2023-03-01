4b7b01a4660112e5df700f06c688f6b49bd28e22
gongpingzz <gongpingzz@foxmail.com>
2023-03-01 23:26:44 +0800

    添加1732单链表题解,时间复杂度beats 100%

--- Leetcode/common_header.h
+++ Leetcode/common_header.h
@@ -1,3 +1,18 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+void printSingleLinkedList(struct ListNode *head){
+    while(head){
+        if (head->next)
+            printf("%d -> ", head->val);
+        else
+            printf("%d -> NULL\n", head->val);
+        head = head->next;
+    }
+}
