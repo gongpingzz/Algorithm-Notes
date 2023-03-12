589c52e0082d6a802dc2576038a8cb8aad4beb03
gongpingzz <gongpingzz@foxmail.com>
2023-03-12 22:54:31 +0800

    添加题解1，采用排序后对比的方法，另外可用哈希表解决，关键是长度为

---
+++ Leetcode/0242.c
@@ -0,0 +1,22 @@
+#include "common_header.h"
+
+int compare_char(const void* _a, const void* _b) {
+    char *a = (char*)_a;
+    char *b = (char*)_b;
+    return (*a - *b);
+}
+
+bool isAnagram(char * s, char * t){
+    int len_s = strlen(s);
+    int len_t = strlen(t);
+    if (len_s != len_t) {
+        return false;
+    }
+
+    qsort(s, len_s, sizeof(char), compare_char);
+    qsort(t, len_s, sizeof(char), compare_char);
+    return strcmp(s, t) == 0;
+}
+
+
+
